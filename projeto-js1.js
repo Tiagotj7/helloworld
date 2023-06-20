56b571df11386ac150c16ab795aea8374dbde02b
Tiagotj7 <tiagowork07@gmail.com>
2023-06-20 18:27:49 -0300

    second commit

---
+++ projeto-js1.js
@@ -0,0 +1,7 @@
+alert('Ola Mundo!!!')
+var nome = prompt('Qual é o seu nome?')
+confirm('É um prazer, ' + nome)
+//Comentário em JS 
+/*Comentário em Js */
+//Number para converter string em numero
+//String para converter numero em string
